898ebc3a3c6bc81752e800f5668c7d16b4e7959f
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-09 13:38:17 +0530

    Create mass123.C

---
+++ mass123.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n,sum=0;
+printf("enter the no");
+scanf("%d",&n);
+for(i=0;i<=n;++i)
+{
+sum=sum+i;}
+printf("the sum i %d",sum);
+getch();
+return0;
+}
+}
