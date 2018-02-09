e5729267b129a64c65baee9c2fc65a5929720dcc
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-09 13:34:19 +0530

    Create mass12345678.C

---
+++ mass12345678.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n;
+  printf("enter the n value");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    printf("hello ");
+  }
+getch();
+return0;
+}
