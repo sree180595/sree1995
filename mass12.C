d8f847e90840940c56657e16557f7a0c66e72c2c
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-09 13:41:25 +0530

    Create mass12.C

---
+++ mass12.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a[]={0,1,2,3,4,5,6,7,8,9};
+int sum=0,n,k,i;
+ printf("enter n value");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+    printf("%d",a[i]);
+  printf("enter K value");
+  scanf("%d",&k);
+  for(i=0;i<=k;K++)
+  {
+    sum=sum+a[i];
+  }
+  printf("%d",sum);
+}
