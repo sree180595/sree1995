1253a3e8e72bf7205f891ca5c586dec64c94e9d3
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-09 14:02:28 +0530

    Create mass123456.C

---
+++ mass123456.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+def main():
+   a=input("")
+   k=0
+   for i in range(2,a//2+1):
+     if(a%i==0):
+        k=k+1
+   if(k<=0):
+    print("Yes")
+   else:
+    print("No")
+if __name__ == '__main__':
+    main()
+    return0
