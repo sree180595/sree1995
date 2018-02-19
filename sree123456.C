42c2605977db44f9814b00e3dedd26d221942cf5
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-19 16:00:32 +0530

    Create sree123456.C

---
+++ sree123456.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+try:
+	s=int(input())
+	e=int(input())
+	for n in range(s,e):
+		temp=n
+		ams=0
+		while(temp!=0):
+			rem=temp%10
+			ams=ams+rem*rem*rem
+			temp=int(temp/10)
+			if n==ams :
+				print(n)
+except :
+	print('invalid')
+  return0
