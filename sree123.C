697b574e0a4e56b73b5de7a705f0735533d59d68
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-19 16:18:23 +0530

    Create sree123.C

---
+++ sree123.C
@@ -0,0 +1,16 @@
+def main():
+
+ def multiple(m, n):
+
+   
+    a = range(n, (m * n)+1, n)
+
+    print (*a)
+
+
+    m = 5
+    n = input("")
+    multiple(m, n)
+if __name__ == '__main__':
+    main()
+    return0
