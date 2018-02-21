cd559ab2c90ed71f96cff0fcbc16707a3635508b
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-21 16:19:41 +0530

    Create mathan1234567.C

---
+++ mathan1234567.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+n=input("enter n value\n")
+sdd=[]
+for i in range(0,n):
+	b=int(input())
+	sdd.append(b)
+for i in range(0,n):
+	print(i,sdd[i])
+  return0
+  }
