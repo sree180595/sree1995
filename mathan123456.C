c14ca7dfad7e2883b89bfc83924109b96fb9e915
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-21 16:14:50 +0530

    Create mathan123456.C

---
+++ mathan123456.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+try:
+	x=int(input())
+	y=int(input())
+	t=max(x,y)
+	while(True):
+		if (t%x==0 and t%y==0) :
+			break
+		t=t+1
+	print(t)
+except:
+print('invalid')
+getch();
+}
