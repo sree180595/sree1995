b0d0a87552f52057275ed309fd113676fefd297f
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-26 16:11:15 +0530

    Create yuvi123456789.C

---
+++ yuvi123456789.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("ENTER THE INPUT INTEGERS:\n");
+	scanf("%d%d",&a,&b);
+	printf("BEFORE SWAPPING:\n");
+	printf("a=%d\nb=%d",a,b);
+	a=a ^ b;
+	b=a ^ b;
+	a=a ^ b;
+	printf("\nAFTER SWAPPING:\n");
+	printf("a=%d\nb=%d",a,b);
+  return();
+	}
