cd6537f8dbeb7dae278a556a2d6004f450b85041
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-26 15:55:13 +0530

    Create yuvi1234.c

---
+++ yuvi1234.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	char a[50];
+	int i,count=0;
+	gets(a);
+	
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i] == ' ')
+		count++;
+		
+		
+	}
+	printf("%d",count);
+	return 0;
+}
