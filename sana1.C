17bce49d2ed76fc4472e798ab9e1e6efba5472a9
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-26 16:18:22 +0530

    Create sana1.C

---
+++ sana1.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a=0,b=1,c,n;
+	printf("ENTER THE NO.OF.ELEMENTS:\n");
+	scanf("%d",&n);
+	printf("%d",b);
+	while(n!=0)
+	{
+		c=a+b;
+		printf("\n%d",c);
+		a=b;
+		b=c;
+		n--;
+    return()
+	}
+	}
