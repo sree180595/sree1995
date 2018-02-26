d39f3979157bef4862a84ba782279ac7a83e58e4
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-26 15:58:10 +0530

    Create yuvi12345.C

---
+++ yuvi12345.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char a[50];
+	int n,i,count=1;
+	printf("ENTER THE INPUT SENTENCE:\n");
+	gets(a);
+	n=strlen(a);
+	printf("\nTHE LENGTH OF SENTENCE IS %d",n);
+	for(i=0;i<n;i++)
+	{
+		if(a[i]=='\.')
+		{
+			count++;
+		}
+	}
+	printf("\nTHE NO.OF.LINES IN THE GIVEN PARAGRAPH ARE %d",count);
+}
