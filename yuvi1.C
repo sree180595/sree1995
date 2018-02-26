ca7f2dc22100c687e047ef5c55215d3255c5d5e5
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-26 15:41:45 +0530

    Create yuvi1.C

---
+++ yuvi1.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+voidmain()
+{
+	int hr[2],mins[2],i,min,hour,diff,res[2];
+	for(i=0;i<2;i++)
+	{
+	    scanf("%d %d",&hr[i],&mins[i]);
+	}
+	for(i=0;i<2;i++)
+	{
+	    res[i]=(hr[i]*60)+mins[i];
+	}
+	if(res[0]>res[1])
+	{
+	    diff=res[0]-res[1];
+	}
+	else
+	{
+	    diff=res[1]-res[0];
+	}
+	hour=diff/60;
+	min=diff%60;
+	printf("%d %d",hour,min);
+  getch();
+	return 0;
+}
