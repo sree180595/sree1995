df0e1cce246ac56ab31ccaa22c2b2435782e86df
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-03-05 15:53:29 +0530

    Create vishnu1.C

---
+++ vishnu1.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main() 
+{
+	int n;
+	scanf("%d",&n);
+    if(n>=1 && n<=10)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    getch();
+	return 0;
+}
