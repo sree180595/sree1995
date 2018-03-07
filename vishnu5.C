f4dc5a9510457ac4be4f06246ca1d0b646f6afcd
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-03-07 15:58:34 +0530

    Create vishnu5.C

---
+++ vishnu5.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	int i,n,array[50],sum=0,avg;
+	printf("enter the size of the array");
+	scanf("%d",&n);
+	printf("enter the array elements");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&array[i]);
+	
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+array[i];
+	}
+	avg=sum/n;
+	printf("\n%d",sum);
+	printf("\n%d",avg);
+  getch();
+	return 0;
+}
