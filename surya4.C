d6af38158bafabad48a7bde2ecec411b69fa3483
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-03-14 03:26:47 -0700

    Create surya4.C

---
+++ surya4.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+
+    char s1[50],s2[50],ch=',';
+    printf("enter the string :");
+    scanf("%[^\n]s",s1);
+    printf("%c %c %c",s1[0],ch,s1[2]);
+getch();
+}
