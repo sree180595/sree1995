27af025b93069edaf2580fce49fc6971beff6518
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-03-23 15:54:34 +0530

    Create 79.C

---
+++ 79.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter the two values\n");
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	if(a==b)
+	{
+		printf("yes\n");
+	}
+	else
+	{
+		printf("no\n");
+		
+	}
+  getch();
+	return 0;
+	}
