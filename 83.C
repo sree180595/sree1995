ee8351044969d7d9ffe685ad039830715efa0070
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-03-23 16:07:45 +0530

    Create 83.C

---
+++ 83.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("Enter the Number :");
+scanf("%d\t%d",&a);
+printf("Enter the Divisor :");
+scanf("%d\t%d",&b);
+c=b%a;
+printf("%d",c);
+getch();
+}
