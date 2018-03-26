c748da01a37854ce4c1c12372ec13882ba11a495
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-03-26 16:11:17 +0530

    Create 112.C

---
+++ 112.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() 
+{
+	int n,k,i,a[10],count;
+	scanf("%d %d",&n,&k);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	    if(a[i]==k)
+	    {
+	        printf("yes");
+	        count=1;
+	    }
+	}
+	if(count!=1)
+	{
+	    printf("no");
+	}
+  getch();
+	return 0;
+}
