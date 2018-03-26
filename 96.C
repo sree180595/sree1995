6087bdd833f513aecfce399df39491f7ea01b98f
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-03-26 16:32:17 +0530

    Create 96.C

---
+++ 96.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+	// your code goes here
+	int n,flag=0,i;
+	scanf("%d",&n);
+	if(n%2!=0 && n>3)
+	{
+		for(i=3;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				flag=1;
+				break;
+				
+			}
+		}
+	}
+	if(flag==1)
+	{
+		printf("composite number");
+	}
+	else
+	printf("not a composite number");
+	return 0;
+}
