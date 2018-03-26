63b553b8a6482f5aad3b23c954bbfdf173884571
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-03-26 15:57:08 +0530

    Create 90.C

---
+++ 90.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char s;
+int n,i;
+scanf("%d",&n);
+scanf("S",&s);
+for(i=0;i<n;i++)
+{
+if(a[i]==0||a[i]==1||a[i]==2||a[i]==3||a[i]==4||a[i]==5||a[i]==6||a[i]==7||a[i]==8||a[i]==9)
+{
+printf("%d",a[i]);
+getch();
+}
+}
